91dc775f6a85fbd35676b931bcb601c7ba359552
houcimi <30775212+houcimi@users.noreply.github.com>
2024-01-31 10:36:01 +0100

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,1 +1,13 @@
-print("first print")
+import streamlit as st
+
+def main():
+    st.title('Streamlit App Test')
+
+    # You can change this message to anything you want
+    test_message = 'Hello, this is a test message from my Streamlit app!'
+    
+    # Displaying the message on the app
+    st.write(test_message)
+
+if __name__ == "__main__":
+    main()
